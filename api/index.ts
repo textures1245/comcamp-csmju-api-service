28b6e0561a8b5bbd3b8813026e14b76eae07ad1c
Traiphakh stihhikaew <sirprak1245@gmail.com>
2023-12-13 20:04:45 +0700

    add: CORS implemented

--- api/index.ts
+++ api/index.ts
@@ -3,11 +3,17 @@ import mailServiceRoute from "./../routes/mail-service";
 import Controller from "../controllers";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
+app.use(
+  cors({
+    origin: [process.env.DEV_LOCAL_PORT ?? "", "https://comcamp.csmju.com"],
+  })
+); // Enable CORS for all routes
 // enable JSON body parser
 app.use(bodyParser.json());
 
