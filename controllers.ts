d322094343822547217e0f515f54251c198bba11
Traiphakh stihhikaew <sirprak1245@gmail.com>
2023-12-13 20:27:37 +0700

    add: add cors headers manually on controller

--- controllers.ts
+++ controllers.ts
@@ -7,6 +7,10 @@ dotenv.config();
 
 const Controller = {
   mailSender: async (req: Request, res: Response) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
     type MailData = {
       to: string;
       from: string;
